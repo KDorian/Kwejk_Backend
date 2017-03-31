5b4e7fff1ca3c792725b0d5c468920d4bba5906b
Dorian Kucharski <dorian.kucharski@gmail.com>
2017-03-31 15:59:47 +0200

    Add static files

--- src/main/java/com/example/Controller/GifController.java
+++ src/main/java/com/example/Controller/GifController.java
@@ -1,6 +1,8 @@
 package com.example.Controller;
 
+import com.example.model.Gif;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -15,5 +17,12 @@ public class GifController {
     @GetMapping("/")
     public String listGifs() {
         return "home";
+    }
+
+    @RequestMapping("/gif")
+    public String gifDetails(ModelMap modelMap){
+        Gif gif = new Gif("compiler-bot","Dorian",true);
+        modelMap.put("gif",gif);
+        return "gif-details";
     }
 }
